1502a10e1798b7f6074e6ce3a207d317e5e22419
Priyanka Sati <69068748+Priyanka-Sati@users.noreply.github.com>
2020-10-20 19:36:29 +0530

    Update ArticleModel.php

--- application/models/ArticleModel.php
+++ application/models/ArticleModel.php
@@ -5,7 +5,7 @@ class ArticleModel extends CI_Model
 {
 
 	function fetch_all_articles(){
-		$resultQuery=$this->db->query("SELECT `blogid`, `blog_title`,`blog_img`,`blog_desc`,`created_on`,`updated_on` FROM `articles` WHERE status='1'");
+		$resultQuery=$this->db->query("SELECT `blogid`, `blog_title`,`blog_img`,`blog_desc`,`created_on`,`updated_on` FROM `articles`");
 
 		return $resultQuery->result_array();
 	}
